6719f55392eb67a507cf9aec05002007ccce7958
Uday Singh <thakuruday721@gmail.com>
2026-06-04 13:26:38 +0530

    Add Welcome Message Generator

---
+++ 1- WelcomeMessageGenerator.py
@@ -0,0 +1,11 @@
+name = input("Enter your name \n")
+hobby = input("Enter your favorite hobby \n")
+age = int(input("What's your age ?? \n"))
+
+#Generating welcome message
+
+print("\n ----- Welcome Message -----")
+print(f" Hello {name}")
+print(" Welcome to this amazing world ")
+print(f" It's great to know that you are {age} only and you like {hobby}")
+print(" Let dive deep into the world of Python")
